f3177407b2b05398a413c8977282fa470e1a6012
ngnawensv <ngnawens@gamil.com>
2021-03-02 20:55:14 +0100

    Ribbon disable in spring boot great than 2.4.0

--- service-article/src/main/java/cm/belrose/servicearticle/proxy/ProxyCategorieService.java
+++ service-article/src/main/java/cm/belrose/servicearticle/proxy/ProxyCategorieService.java
@@ -1,15 +1,15 @@
 package cm.belrose.servicearticle.proxy;
 
 import cm.belrose.servicearticle.dto.Categorie;
+import org.springframework.cloud.netflix.ribbon.RibbonClient;
 import org.springframework.cloud.openfeign.FeignClient;
-import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
 
 @FeignClient(name = "service-categorie") //specification du service  à appelé
+//@RibbonClient(name="service-categorie")
 public interface ProxyCategorieService {
 
     @GetMapping(value = "/categories/{id}")
